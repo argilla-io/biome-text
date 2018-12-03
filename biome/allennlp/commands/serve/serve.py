f86a8187e6696687a818935e5b8bd2086b0499d5
Francisco Aranda <francisco@recogn.ai>
2018-12-03 16:17:19 +0100

    chore: use default allennlp server

--- biome/allennlp/commands/serve/serve.py
+++ biome/allennlp/commands/serve/serve.py
@@ -1,8 +1,15 @@
 import argparse
+import logging
 
 from allennlp.commands import Subcommand
+from allennlp.models import load_archive
+from allennlp.service import server_simple
+from flask_cors import CORS
+from gevent.pywsgi import WSGIServer
 
-from . import server_sanic
+from biome.allennlp.predictors.utils import get_predictor_from_archive
+
+logger = logging.getLogger(__name__)
 
 
 class BiomeRestAPI(Subcommand):
@@ -12,7 +19,6 @@ class BiomeRestAPI(Subcommand):
         subparser = parser.add_parser(name, description=description, help='Run the web service.')
 
         subparser.add_argument('--port', type=int, default=8000)
-        subparser.add_argument('--workers', type=int, default=1)
         subparser.add_argument('--binary', type=str, required=True)
 
         subparser.set_defaults(func=_serve)
@@ -21,4 +27,14 @@ class BiomeRestAPI(Subcommand):
 
 
 def _serve(args: argparse.Namespace) -> None:
-    server_sanic.run(args.port, args.workers, model_archive=args.binary)
+    model_archive = load_archive(args.binary)
+    # Matching predictor name with model name
+    predictor = get_predictor_from_archive(model_archive)
+
+    # server_sanic.run(args.port, args.workers, model_archive=args.binary)
+    app = server_simple.make_app(predictor, title=predictor.__class__.__name__)
+    CORS(app)
+
+    http_server = WSGIServer(('0.0.0.0', args.port), app)
+    logger.info(f"Model loaded, serving demo on port {args.port}")
+    http_server.serve_forever()
