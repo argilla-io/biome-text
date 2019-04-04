bead5a1d3fe80322b7875a67421fbe9738fbed0f
Francisco Aranda <francisco@recogn.ai>
2019-04-04 18:42:04 +0200

    test: include test for biluo ds reader

--- tests/__init__.py
+++ tests/__init__.py
@@ -5,3 +5,4 @@ logging.basicConfig(level=logging.INFO)
 test_logger = logging.getLogger(__name__)
 
 TESTS_BASEPATH = os.path.dirname(__file__)
+TEST_RESOURCES = os.path.join(TESTS_BASEPATH, 'resources')
---
+++ tests/allennlp/dataset_readers/biluo_tagger_ds_reader_test.py
@@ -0,0 +1,24 @@
+import os
+import unittest
+
+from biome.allennlp.dataset_readers.biluo_tagger_ds_reader import (
+    BiluoSequenceTaggerDatasetReader,
+)
+from tests import TEST_RESOURCES
+
+
+class BiluoSequenceTaggerDatasetReaderTest(unittest.TestCase):
+
+    reader = BiluoSequenceTaggerDatasetReader(lazy=False)
+    data_path = os.path.join(TEST_RESOURCES, "data/biluo.data.json")
+    expected_dataset_size = 3797
+
+    def test_read_biluo_dataset(self):
+        instances = self.reader.read(self.data_path)
+        instances = list(instances)
+
+        self.assertEqual(self.expected_dataset_size, len(instances))
+
+        for instance in instances:
+            self.assertTrue("tokens" in instance.fields)
+            self.assertTrue("tags" in instance.fields)
