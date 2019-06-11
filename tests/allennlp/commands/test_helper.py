84d949d85990b39febe45b3ef37293f54861af0b
David Fidalgo <david@recogn.ai>
2019-06-11 17:38:48 +0200

    adapt tests

--- tests/allennlp/commands/test_helper.py
+++ tests/allennlp/commands/test_helper.py
@@ -1,7 +1,7 @@
 import os
 import unittest
 
-from biome.allennlp.commands.helpers import biome2allennlp_params, VALIDATION_DATA_FIELD
+from biome.allennlp.commands.helpers import biome2allennlp_params, VALIDATION_DATA_FIELD, BiomeConfig
 
 from tests.test_context import TEST_RESOURCES
 
@@ -11,14 +11,14 @@ SPEC_PATH = os.path.join(TEST_RESOURCES, 'resources/models/bert.classifier.yml')
 
 class TestCommandHelper(unittest.TestCase):
 
-    def test_biome2allennlp_params_cuda_env_overrides(self):
-        cfg = biome2allennlp_params(model_spec=SPEC_PATH, trainer_path=WITHOUT_CUDA_DEVICE_TRAINER_PATH)
+    def test_BiomeConfig_cuda_env_overrides(self):
+        cfg = BiomeConfig(model_path=SPEC_PATH, trainer_path=WITHOUT_CUDA_DEVICE_TRAINER_PATH).to_allennlp_params()
         trainer_cfg = cfg['trainer']
 
         assert trainer_cfg['cuda_device'] == -1, 'Wrong cuda device expected'
 
         os.environ['CUDA_DEVICE'] = str(3)
-        cfg = biome2allennlp_params(model_spec=SPEC_PATH, trainer_path=WITHOUT_CUDA_DEVICE_TRAINER_PATH)
+        cfg = BiomeConfig(model_path=SPEC_PATH, trainer_path=WITHOUT_CUDA_DEVICE_TRAINER_PATH).to_allennlp_params()
         trainer_cfg = cfg['trainer']
 
         assert trainer_cfg['cuda_device'] == int(os.getenv('CUDA_DEVICE')), 'Wrong cuda device expected'
