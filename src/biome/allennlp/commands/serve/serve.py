17ba87a276867347cbeb476dcbe68c0185be1ba7
Francisco Aranda <francisco@recogn.ai>
2019-06-14 21:38:34 +0200

    feat(serve): allow expose serve app for gunicorn integration

--- src/biome/allennlp/commands/serve/serve.py
+++ src/biome/allennlp/commands/serve/serve.py
@@ -7,6 +7,7 @@ from allennlp.service import server_simple
 from biome.allennlp.predictors import get_predictor_from_archive
 from flask_cors import CORS
 from gevent.pywsgi import WSGIServer
+from typing import Optional
 
 logger = logging.getLogger(__name__)
 
@@ -40,13 +41,30 @@ def _serve_from_args(args: argparse.Namespace) -> None:
     return serve(binary=args.binary, port=args.port, predictor=args.predictor)
 
 
-def serve(binary: str, port: int = 8000, predictor: str = None) -> None:
-    model_archive = load_archive(binary)
-    # Matching predictor name with model name
-    predictor = get_predictor_from_archive(model_archive, predictor_name=predictor)
+def make_app(binary: str, predictor_name: Optional[str] = None):
+    """
+    This function allows serve model from gunicorn server. For example:
 
-    app = server_simple.make_app(predictor, title=predictor.__class__.__name__)
+    >>>gunicorn 'biome.allennlp.commands.serve.serve:make_app("/path/to/model.tar.gz")'
+
+    :param binary: the model.tar.gz path
+    :param predictor_name: optional predictor to use
+    :return: a Flask app used by gunicorn server
+    """
+    model_archive = load_archive(binary)
+    # Matching predictor_name name with model name
+    predictor_name = get_predictor_from_archive(
+        model_archive, predictor_name=predictor_name
+    )
+    app = server_simple.make_app(
+        predictor_name, title=predictor_name.__class__.__name__
+    )
     CORS(app)
+    return app
+
+
+def serve(binary: str, port: int = 8000, predictor: str = None) -> None:
+    app = make_app(binary, predictor)
 
     http_server = WSGIServer(("0.0.0.0", port), app)
     logger.info(f"Model loaded, serving on port {port}")
