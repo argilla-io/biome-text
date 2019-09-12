e4af7ae9557510bb296663e8f7afb291b6d70219
Francisco Aranda <francisco@recogn.ai>
2019-09-12 09:01:30 +0200

    chore: skip test and refactor

--- tests/data/sources/test_excel_source.py
+++ tests/data/sources/test_excel_source.py
@@ -1,35 +1,36 @@
-import json
 import os
 import tempfile
 
 from tests import TESTS_BASEPATH
 from tests.test_support import DaskSupportTest
 
-FILES_PATH = os.path.join(TESTS_BASEPATH, 'resources/files')
+FILES_PATH = os.path.join(TESTS_BASEPATH, "resources/files")
 
 from biome.data.sources import DataSource
 from biome.data.sinks.helpers import store_dataset
 
 
 class ExcelDatasourceTest(DaskSupportTest):
-
     def test_read_and_write_excel(self):
-        file_path = os.path.join(FILES_PATH, 'test.xlsx')
+        def drop_keys(data, keys):
+            return {k: v for k, v in data.items() if k not in keys}
 
-        datasource = DataSource(format='xlsx', path=file_path)
+        file_path = os.path.join(FILES_PATH, "test.xlsx")
+
+        datasource = DataSource(format="xlsx", path=file_path)
 
         tmpfile = tempfile.mkdtemp()
         store_dataset(datasource.to_bag(), dict(path=tmpfile))
 
-        stored_dataset = DataSource(format='json', path=os.path.join(tmpfile, '*.part'))
+        stored_dataset = DataSource(format="json", path=os.path.join(tmpfile, "*.part"))
 
         stored = stored_dataset.to_bag().compute()
         read = datasource.to_bag().compute()
 
-        assert len(stored) == len(read)
+        self.assertEqual(len(stored), len(read))
 
-        drop_keys = lambda data, keys: {k: v for k, v in data.items() if k not in keys}
-        variable_keys = ['resource', 'id']
-
-        for a, b in zip(read, stored):
-            assert drop_keys(a, variable_keys) == drop_keys(a, variable_keys)
+        variable_keys = ["resource", "id"]
+        [
+            self.assertEqual(drop_keys(a, variable_keys), drop_keys(a, variable_keys))
+            for a, b in zip(read, stored)
+        ]
