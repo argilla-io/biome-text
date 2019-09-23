35eea893f72482efd4a4d49c9535dbd84b51a739
David Fidalgo <david@recogn.ai>
2019-09-23 12:01:36 +0200

    bugfix ... sorry about that!

--- src/biome/text/__init__.py
+++ src/biome/text/__init__.py
@@ -1,5 +1,8 @@
 import pkg_resources
 
+# This is necessary, since the from_param machinery needs our classes to be registered!
+from . import dataset_readers, models, predictors
+
 try:
     __version__ = pkg_resources.get_distribution(__name__.replace(".", "-")).version
 except pkg_resources.DistributionNotFound:
