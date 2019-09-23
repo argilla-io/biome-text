c395fb27d71fe48e857095f9004cadefaf298db8
David Fidalgo <david@recogn.ai>
2019-09-23 11:37:48 +0200

    delete init since Python 3.3 added implicit namespace packages + add version information

--- src/biome/__init__.py
+++
@@ -1,3 +0,0 @@
-# https://packaging.python.org/guides/packaging-namespace-packages/#pkgutil-style-namespace-packages
-__path__ = __import__("pkgutil").extend_path(__path__, __name__)
-__version__ = "0.1.dev"
--- src/biome/text/__init__.py
+++ src/biome/text/__init__.py
@@ -1,1 +1,7 @@
-from . import commands, dataset_readers, models, predictors
+import pkg_resources
+
+try:
+    __version__ = pkg_resources.get_distribution(__name__.replace(".", "-")).version
+except pkg_resources.DistributionNotFound:
+    # package is not installed
+    pass
