1a3afecc59c39125030403c5040fb92dd2fe515f
Francisco Aranda <francisco@recogn.ai>
2019-09-25 16:03:47 +0200

    fix: sort import properly for disable warnings

--- src/biome/text/__init__.py
+++ src/biome/text/__init__.py
@@ -3,9 +3,6 @@ from warnings import warn_explicit
 
 import pkg_resources
 
-# This is necessary, since the from_param machinery needs our classes to be registered!
-from . import dataset_readers, models, predictors
-
 warnings.showwarning = warn_explicit
 
 try:
@@ -13,3 +10,6 @@ try:
 except pkg_resources.DistributionNotFound:
     # package is not installed
     pass
+
+# This is necessary, since the from_param machinery needs our classes to be registered!
+from . import dataset_readers, models, predictors
