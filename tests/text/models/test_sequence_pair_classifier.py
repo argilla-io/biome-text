58b026740cef9a374901724bdcaf82d6f66d74c3
Francisco Aranda <frascuchon@gmail.com>
2019-10-02 17:45:32 +0200

    test: sequence pair classifier workflow test (#54)
    * test: sequence pair classifier training test
    
    * test: test train, predict and serve for sequence pair classifier
    
    * chore: add elasticsearch as service

--- tests/text/models/test_sequence_pair_classifier.py
+++ tests/text/models/test_sequence_pair_classifier.py
@@ -1,51 +1,86 @@
 import logging
-import unittest
+import os
+import tempfile
+import threading
+from time import sleep
+
+import requests
+from biome.data.utils import ENV_ES_HOSTS
+from elasticsearch import Elasticsearch
+
+from biome.text.commands.learn.learn import learn
+from biome.text.commands.predict.predict import predict
+from biome.text.commands.serve.serve import serve
+from biome.text.models import SequencePairClassifier, load_archive
+from tests.test_context import TEST_RESOURCES
+from tests.test_support import DaskSupportTest
 
 logging.basicConfig(level=logging.DEBUG)
 
-import os
-import tempfile
 
-from allennlp.common import Params
-
-from biome.text.commands.learn import learn
-from biome.text.models import SequencePairClassifier
-from tests.test_support import DaskSupportTest
-from tests.test_context import TEST_RESOURCES
-
-DEFINITION_TRAIN = os.path.join(
-    TEST_RESOURCES, "resources/definitions/train/train_sequence_pair_classifier.json"
-)
-TRAINER_PATH = os.path.join(TEST_RESOURCES, "resources/definitions/train/trainer.json")
-TRAIN_DATA_PATH = os.path.join(
-    TEST_RESOURCES, "resources/definitions/train/train.data.json"
-)
-VALIDATION_DATA_PATH = os.path.join(
-    TEST_RESOURCES, "resources/definitions/train/validation.data.json"
+BASE_CONFIG_PATH = os.path.join(
+    TEST_RESOURCES, "resources/definitions/sequence_pair_classifier"
 )
 
 
-# TODO @dvilasuero check this test when SequenceClassifier refactor is done
-@unittest.skip(reason="SequenceClassifier refactor must be finish")
-class TrainSeqListClassifierTest(DaskSupportTest):
-    @staticmethod
-    def test_train_model_from_file():
-        output_dir = tempfile.mkdtemp()
-        serialization_dir = os.path.join(output_dir, "test")
+class SequencePairClassifierTest(DaskSupportTest):
+    output_dir = tempfile.mkdtemp()
+    model_archive = os.path.join(output_dir, "model.tar.gz")
+
+    name = "sequence_pair_classifier"
+    model_path = os.path.join(BASE_CONFIG_PATH, "model.yml")
+    trainer_path = os.path.join(BASE_CONFIG_PATH, "trainer.yml")
+    training_data = os.path.join(BASE_CONFIG_PATH, "train.data.yml")
+    validation_data = os.path.join(BASE_CONFIG_PATH, "validation.data.yml")
+
+    def test_model_workflow(self):
+        self.check_train(SequencePairClassifier)
+        self.check_predict()
+        self.check_serve()
+
+    def check_train(self, cls_type):
+
         _ = learn(
-            model_spec=DEFINITION_TRAIN,
-            output=serialization_dir,
-            train_cfg=TRAIN_DATA_PATH,
-            validation_cfg=VALIDATION_DATA_PATH,
-            trainer_path=TRAINER_PATH,
+            model_spec=self.model_path,
+            output=self.output_dir,
+            train_cfg=self.training_data,
+            validation_cfg=self.validation_data,
+            trainer_path=self.trainer_path,
         )
+        archive = load_archive(self.model_archive)
+        self.assertTrue(archive.model is not None)
+        self.assertIsInstance(archive.model, cls_type)
 
-        model = SequencePairClassifier.from_params(
-            params=Params(
-                {"model_location": os.path.join(serialization_dir, "model.tar.gz")}
+    def check_predict(self):
+        index = self.name
+        es_host = os.getenv(ENV_ES_HOSTS, "http://localhost:9200")
+        predict(
+            binary=self.model_archive,
+            from_source=self.validation_data,
+            to_sink=dict(
+                index=index,
+                index_recreate=True,
+                type="doc",
+                es_hosts=es_host,
+                es_batch_size=100,
             ),
-            vocab=None,
         )
 
-        assert model
-        assert isinstance(model, SequencePairClassifier)
+        client = Elasticsearch(hosts=es_host, http_compress=True)
+        data = client.search(index)
+        self.assertIn("hits", data, msg=f"Must exists hits in es response {data}")
+        self.assertTrue(len(data["hits"]) > 0, "No data indexed")
+
+    def check_serve(self):
+        port = 8000
+        thread = threading.Thread(
+            target=serve, daemon=True, kwargs=dict(binary=self.model_archive, port=port)
+        )
+        thread.start()
+        sleep(5)
+
+        response = requests.post(
+            f"http://localhost:{port}/predict",
+            json={"record1": "mike Farrys", "record2": "Mike Farris"},
+        )
+        self.assertTrue(response.json() is not None)
