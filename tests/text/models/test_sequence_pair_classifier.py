bee9c3f04e09531ea7443f7ae867dd2689ff0f90
Francisco Aranda <francisco@recogn.ai>
2019-10-02 17:06:49 +0200

    test: test train, predict and serve for sequence pair classifier

--- tests/text/models/test_sequence_pair_classifier.py
+++ tests/text/models/test_sequence_pair_classifier.py
@@ -1,11 +1,17 @@
 import logging
 import os
 import tempfile
+import threading
+from time import sleep
 
-from allennlp.models import load_archive
+import requests
+from biome.data.utils import ENV_ES_HOSTS
+from elasticsearch import Elasticsearch
 
 from biome.text.commands.learn.learn import learn
-from biome.text.models import SequencePairClassifier
+from biome.text.commands.predict.predict import predict
+from biome.text.commands.serve.serve import serve
+from biome.text.models import SequencePairClassifier, load_archive
 from tests.test_context import TEST_RESOURCES
 from tests.test_support import DaskSupportTest
 
@@ -27,18 +33,57 @@ VALIDATION_DATA_PATH = os.path.join(
 
 
 class SequencePairClassifierTest(DaskSupportTest):
-    def test_train_model_from_file(self):
-        output_dir = tempfile.mkdtemp()
-        serialization_dir = os.path.join(output_dir, "test")
+    output_dir = tempfile.mkdtemp()
+    model_archive = os.path.join(output_dir, "model.tar.gz")
+
+    def test_model_workflow(self):
+        self.check_train()
+        self.check_predict()
+        self.check_serve()
+
+    def check_train(self):
+
         _ = learn(
             model_spec=DEFINITION_TRAIN,
-            output=serialization_dir,
+            output=self.output_dir,
             train_cfg=TRAIN_DATA_PATH,
             validation_cfg=VALIDATION_DATA_PATH,
             trainer_path=TRAINER_PATH,
         )
-
-        archive = load_archive(os.path.join(serialization_dir, "model.tar.gz"))
-
+        archive = load_archive(self.model_archive)
         self.assertTrue(archive.model is not None)
         self.assertIsInstance(archive.model, SequencePairClassifier)
+
+    def check_predict(self):
+        index = "sequence_classifier_prediction"
+        es_host = os.getenv(ENV_ES_HOSTS, "http://localhost:9200")
+        predict(
+            binary=self.model_archive,
+            from_source=VALIDATION_DATA_PATH,
+            to_sink=dict(
+                index=index,
+                index_recreate=True,
+                type="doc",
+                es_hosts=es_host,
+                es_batch_size=100,
+            ),
+        )
+
+        client = Elasticsearch(hosts=es_host, http_compress=True)
+        data = client.search(index)
+        self.assertIn("hits", data, msg=f"Must exists hits in es response {data}")
+        self.assertTrue(len(data["hits"]) > 0, "No data indexed")
+
+    def check_serve(self):
+        port = 8000
+        thread = threading.Thread(
+            target=serve, daemon=True, kwargs=dict(binary=self.model_archive, port=port)
+        )
+        thread.start()
+        sleep(5)
+
+        response = requests.post(
+            f"http://localhost:{port}/predict",
+            json={"record1": "mike Farrys", "record2": "Mike Farris"},
+        )
+        self.assertTrue(response.json() is not None)
