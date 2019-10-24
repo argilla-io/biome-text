a6a9e870a5d9cbb951d588b561e686befd91994c
Francisco Aranda <frascuchon@gmail.com>
2019-10-24 09:57:26 +0200

    General biome ui command (#71)
    * new biome ui command
    
    * configure package data for ui statics
    
    * make directive for upgrade classifier ui from gitlab artifacts
    
    * feat(ui): allow proxy elasticsearch PUT requests

--- src/biome/text/commands/ui/ui.py
+++ src/biome/text/commands/ui/ui.py
@@ -16,7 +16,7 @@ logging.basicConfig(level=logging.INFO)
 _logger = logging.getLogger(__name__)
 
 
-class BiomeExplore(Subcommand):
+class BiomeUI(Subcommand):
     def add_subparser(
         self, name: str, parser: argparse._SubParsersAction
     ) -> argparse.ArgumentParser:
@@ -26,28 +26,33 @@ class BiomeExplore(Subcommand):
             name, description=description, help="Explore your data"
         )
         subparser.add_argument(
+            "--es-host", help="The elasticsearch backend storage", default=None
+        )
+        subparser.add_argument(
             "--port",
             help="Listening port for application",
             default=9000,
             type=lambda a: int(a),
         )
-        subparser.set_defaults(func=_explore_from_args)
+        subparser.set_defaults(func=launch_ui_from_args)
         return subparser
 
 
-def _explore_from_args(args: argparse.Namespace) -> None:
-    return explore(port=args.port)
+def launch_ui_from_args(args: argparse.Namespace) -> None:
+    return launch_ui(args.es_host, port=args.port)
 
 
-def explore(port: int = 9000) -> None:
-    es_host = os.getenv(ENV_ES_HOSTS, "http://localhost:9200")
+def launch_ui(es_host: str, port: int = 9000) -> None:
+    es_host = es_host if es_host else os.getenv(ENV_ES_HOSTS, "http://localhost:9200")
 
     flask_app = make_app(
         es_host=es_host, statics_dir=temporal_static_path("classifier")
     )
 
     http_server = WSGIServer(("0.0.0.0", port), flask_app)
-    _logger.info("Running on http://localhost:{}".format(http_server.server_port))
+    _logger.info(
+        f"Running biome UI on http://0.0.0.0:{http_server.server_port} with elasticsearch backend {es_host}"
+    )
     http_server.serve_forever()
 
 
@@ -55,7 +60,7 @@ def temporal_static_path(explore_view: str, basedir: Optional[str] = None):
     statics_tmp = mkdtemp()
 
     compressed_ui = os.path.join(
-        basedir or os.path.dirname(__file__), "ui", "{}.tar.gz".format(explore_view)
+        basedir or os.path.dirname(__file__), "{}.tar.gz".format(explore_view)
     )
     tar_file = tarfile.open(compressed_ui, "r:gz")
     tar_file.extractall(path=statics_tmp)
