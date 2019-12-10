980521fde6e9f887a7f99a4670c63970f9b0720e
David Fidalgo <david@recogn.ai>
2019-12-10 23:16:49 +0100

    add biome_bimpm model

--- src/biome/text/models/__init__.py
+++ src/biome/text/models/__init__.py
@@ -2,4 +2,5 @@ from .base_model_classifier import BaseModelClassifier
 from .sequence_classifier import SequenceClassifier
 from .sequence_pair_classifier import SequencePairClassifier
 from .similarity_classifier import SimilarityClassifier
+from .biome_bimpm import BiomeBiMpm
 from .archival import load_archive, to_local_archive
