0a09835a86ea6814fd3fbc3175644ed4eeb63905
Francisco Aranda <francisco@recogn.ai>
2020-05-05 16:33:15 +0200

    update examples

--- examples/2.document_classifier/document_classifier.py
+++ examples/2.document_classifier/document_classifier.py
@@ -1,10 +1,13 @@
 from biome.text.api_new import Pipeline
-from biome.text.api_new.configuration import TrainerConfiguration
+from biome.text.api_new.configuration import TrainerConfiguration, VocabularyConfiguration
 from biome.text.api_new.helpers import yaml_to_dict
 
 if __name__ == "__main__":
 
-    pl = Pipeline.from_file("document_classifier.yaml")
+    pl = Pipeline.from_file(
+        "document_classifier.yaml",
+        vocab_config=VocabularyConfiguration(from_path="experiment/vocabulary")
+    )
     print(
         pl.predict(
             document=["Header main. This is a test body!!!", "The next phrase is here"]
