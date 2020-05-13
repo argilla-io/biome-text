bca08b658d78696871a0abbdfe7cd3cf491645b1
LeireA <leire@recogn.ai>
2020-05-13 14:55:41 +0200

    Algolia
    See recognai-team/team#110

--- documentation/docs/.vuepress/config.js
+++ documentation/docs/.vuepress/config.js
@@ -22,45 +22,46 @@ module.exports = {
     '@goy/svg-icons',
     '@vuepress/back-to-top'
   ],
-    themeConfig: {
-        sidebarDepth: 4,
-        nav: [
-          { text: 'API', link: '/api/'},
-          { text: 'Documentation', link: '/documentation/'},
-          { text: 'Github', link: 'https://github.com/recognai/biome-text' },
-          { text: 'Recognai', link: 'https://recogn.ai' },
-        ],
-        sidebar: {
-            '/api/': [{
-                title: 'API',
-                children: getSidebarChildren('docs/api'),
-                collapsable: false
-            }],
-            '/documentation/': [
-            {
-                title: 'Get started',
-                children: ['', 'concepts.md'],
-                collapsable: false
-            },
-            { 
-                title: 'Tutorials', 
-                children:getSidebarChildren('docs/documentation/tutorials', 'docs/documentation'),
-                collapsable: false
-            },
-            {
-                title: 'User Guides',
-                children:getSidebarChildren('docs/documentation/user-guides', 'docs/documentation'), 
-                collapsable: false
-            },
-            {
-                title: 'Community', 
-                children:getSidebarChildren('docs/documentation/community', 'docs/documentation'), 
-                collapsable: false
-            }
-    
-        ]
-        },
+  themeConfig: {
+    sidebarDepth: 4,
+    nav: [
+      { text: 'API', link: '/api/'},
+      { text: 'Documentation', link: '/documentation/'},
+      { text: 'Github', link: 'https://github.com/recognai/biome-text' },
+      { text: 'Recognai', link: 'https://recogn.ai' },
+    ],
+    sidebar: {
+      '/api/': [{
+          title: 'API',
+          children: getSidebarChildren('docs/api'),
+          collapsable: false
+      }],
+      '/documentation/': [
+      {
+          title: 'Get started',
+          children: ['', 'concepts.md'],
+          collapsable: false
+      },
+      { 
+          title: 'Tutorials', 
+          children:getSidebarChildren('docs/documentation/tutorials', 'docs/documentation'),
+          collapsable: false
+      },
+      {
+          title: 'User Guides',
+          children:getSidebarChildren('docs/documentation/user-guides', 'docs/documentation'), 
+          collapsable: false
+      },
+      {
+          title: 'Community', 
+          children:getSidebarChildren('docs/documentation/community', 'docs/documentation'), 
+          collapsable: false
+      }]
+    },
+    algolia: {
+      apiKey: '4f8d6b27d633951bde8c33e391ea6a4d',
+      indexName: 'recogn_biome-text'
+    },
     plugins: ['@vuepress/active-header-links'],
-    // logo: '/assets/img/recognai.png',
   }
 }
