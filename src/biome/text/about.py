4def28aca2e03215b0a989f9e650033f701a1aa7
Francisco Aranda <francisco@recogn.ai>
2020-05-14 08:46:17 +0200

    chore(build): extract release version from releases branches

--- src/biome/text/about.py
+++ src/biome/text/about.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 
-__version__ = "1.0.0.rc"
+__version__ = "1.0.0.rc0"
+
+from typing import Optional
 
 
 def package_version(version: str):
@@ -24,6 +26,22 @@ def package_version(version: str):
         ).split("\n")
         return tags[0]
 
+    def get_version_from_branch(repository: git.Git) -> Optional[str]:
+        """Extract version from release branch name"""
+        current_branch = repository.rev_parse("--abbrev-ref", "HEAD")
+
+        dropped_releases = current_branch.replace("releases/", "")
+        if current_branch == dropped_releases:
+            return None
+        return dropped_releases
+
+    def version_matches(release_branch_version:str, configured_version:str)->bool:
+        """Checks if a version matches with related release branch version"""
+        minor_release = tuple(release_branch_version.split(".")[:2])
+        minor_configured = tuple(configured_version.split(".")[:2])
+
+        return minor_release == minor_configured
+
     try:
         repo = git.Git()
     except Exception:  # pylint: disable=broad-except
@@ -31,6 +49,10 @@ def package_version(version: str):
 
     commit = get_commit_hash(repo)
     repo_tag = get_first_tag_for_commit(repo, commit)
+    release_version = get_version_from_branch(repo)
+    if release_version and not repo_tag:
+        assert version_matches(release_version, version)
+        return version
     today = datetime.today().strftime("%Y%m%d%H%M%S")
 
     return repo_tag if repo_tag else f"{version}.{today}+{commit}"
