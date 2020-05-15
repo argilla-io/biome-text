d8cf36b5b1cfd848f8857b1a56c897faa7556e16
Francisco Aranda <francisco@recogn.ai>
2020-05-15 09:23:59 +0200

    feat: add tokenization info on explain

--- src/biome/text/_model.py
+++ src/biome/text/_model.py
@@ -10,14 +10,15 @@ from copy import deepcopy
 from functools import lru_cache
 from logging.handlers import RotatingFileHandler
 from pathlib import Path
-from typing import Any, Dict, Iterable, List, Optional, Tuple, Type
+from typing import Any, Dict, Iterable, List, Optional, Tuple, Type, Union, cast
 
 import allennlp
 import numpy
 import torch
 from allennlp.common import Params
 from allennlp.common.util import prepare_environment, sanitize
-from allennlp.data import DataLoader, Instance, Vocabulary
+from allennlp.data import DataLoader, DatasetReader, Field, Instance, Vocabulary
+from allennlp.data.fields import ListField, TextField
 from allennlp.models.archival import CONFIG_NAME, archive_model
 from allennlp.training import Trainer
 from allennlp.training.util import evaluate
@@ -28,7 +29,7 @@ from biome.text import vocabulary
 from .backbone import ModelBackbone
 from .configuration import PipelineConfiguration
 from .data import DataSource
-from .errors import MissingArgumentError
+from .errors import MissingArgumentError, WrongValueError
 from .features import WordFeatures
 from .helpers import split_signature_params_by_predicate
 from .modules.heads import TaskHead, TaskOutput
@@ -284,9 +285,32 @@ class PipelineModel(allennlp.models.Model, allennlp.data.DatasetReader):
         """Applies an prediction including token attribution explanation"""
         inputs = self._model_inputs_from_args(*args, **kwargs)
         instance = self.text_to_instance(**inputs)
-        prediction = self.forward_on_instance(instance)
+        tokenization = self._get_instance_tokenization(instance)
 
-        return self._head.explain_prediction(prediction=prediction, instance=instance)
+        prediction = self.forward_on_instance(instance)
+        explain = self._head.prediction_explain(prediction=prediction, instance=instance)
+        # TODO: change explain data model include both, tokenization and attributions information
+        #  We must apply this change in sync with frontend team.
+        #  The data model proposal could be as follow:
+        #    {
+        #        "tokenization": { "text": ["This", "is", "an", "example"] },
+        #        "attributions": { "text": [{"token": "This", "attribution": 0.39},...]}
+        #    }
+        #   For now, we add tokenization as a new separate field
+        explain.update({"tokenization": tokenization})
+
+        return explain
+
+    def _get_instance_tokenization(self, instance: Instance)->Dict[str, Any]:
+        """Gets the tokenization information to current instance"""
+        def extract_field_tokens(field: Field) -> Union[List[str], List[List[str]]]:
+            """Tries to extract tokens from field"""
+            if isinstance(field, TextField):
+                return [token.text for token in cast(TextField, field).tokens]
+            if isinstance(field, ListField):
+                return [extract_field_tokens(inner_field) for inner_field in cast(ListField, field)]
+            raise WrongValueError(f"Cannot extract fields from [{type(field)}]")
+        return {name: extract_field_tokens(field) for name, field in instance.items()}
 
     def _model_inputs_from_args(self, *args, **kwargs) -> Dict[str, Any]:
         """Returns model input data dictionary"""
