54a6ad78f096d07eb7361978187838105d64e302
David Fidalgo <david@recogn.ai>
2020-05-21 14:09:50 +0200

    add the training_a_test_classifier.ipynb tutorial

--- tests/tutorials/test_tutorials.py
+++ tests/tutorials/test_tutorials.py
@@ -1,9 +1,9 @@
 from pytest_notebook.nb_regression import NBRegressionFixture
 from tests import TUTORIALS_PATH
-from pathlib import Path
+import os
 
 
-def test_notebook():
+def test_text_classifier_tutorial():
     fixture = NBRegressionFixture(exec_timeout=50)
     fixture.diff_ignore = ("/cells/*/execution_count",)
-    fixture.check(str(Path(TUTORIALS_PATH) / "test.ipynb"))
+    fixture.check(os.path.join(TUTORIALS_PATH, "Training_a_text_classifier.ipynb"))
