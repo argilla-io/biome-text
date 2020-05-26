5aa580ef45f2794c0bad8c23b7d04018f7145d69
Francisco Aranda <francisco@recogn.ai>
2020-05-26 14:42:31 +0200

    using allennlp SoftmaxLoss module

--- src/biome/text/modules/heads/language_modelling.py
+++ src/biome/text/modules/heads/language_modelling.py
@@ -1,44 +1,16 @@
 from typing import Dict, Optional
 
-import numpy as np
 import torch
 from allennlp.common.checks import ConfigurationError
 from allennlp.data import Instance, TextFieldTensors
+from allennlp.modules import SoftmaxLoss
 from allennlp.nn.util import get_text_field_mask, get_token_ids_from_text_field_tensors
 from allennlp.training.metrics import Perplexity
-from biome.text.features import WordFeatures
 
 from biome.text.backbone import ModelBackbone
 from biome.text.modules.specs import ComponentSpec
 from biome.text import vocabulary
 from .defs import TaskHead, TaskName, TaskOutput
-
-
-class SoftmaxLoss(torch.nn.Module):
-    """
-    Given some embeddings and some targets, applies a linear layer
-    to create logits over possible words and then returns the
-    negative log likelihood.
-    TODO: copied from allennlp master branch, remove when 1.0 is released
-    """
-
-    def __init__(self, num_words: int, embedding_dim: int) -> None:
-        super(SoftmaxLoss, self).__init__()
-
-        self.softmax_w = torch.nn.Parameter(
-            torch.randn(embedding_dim, num_words) / np.sqrt(embedding_dim)
-        )
-        self.softmax_b = torch.nn.Parameter(torch.zeros(num_words))
-
-    def forward(self, embeddings: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
-        # embeddings is size (n, embedding_dim)
-        # targets is (batch_size, ) with the correct class id
-        # Does not do any count normalization / divide by batch size
-        probs = torch.nn.functional.log_softmax(
-            torch.matmul(embeddings, self.softmax_w) + self.softmax_b, dim=-1
-        )
-
-        return torch.nn.functional.nll_loss(probs, targets.long(), reduction="sum")
 
 
 class LanguageModelling(TaskHead):
