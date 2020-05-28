69d0742505d76e6d3d80656c915e15b11da1e23e
LeireA <leire@recogn.ai>
2020-05-28 15:20:23 +0200

    fix path to styles

--- ui/src/components/common/Explain.vue
+++ ui/src/components/common/Explain.vue
@@ -73,7 +73,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import "@recognai/re-commons/src/assets/scss/apps/classifier/explain.scss";
+@import "@/assets/scss/apps/classifier/explain.scss";
 .token {
   &__container {
     display: inline-block;
