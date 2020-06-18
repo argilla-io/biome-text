10cecab1b92f6899dde49eac8b051cda83f4e9f3
LeireA <leire@recogn.ai>
2020-06-18 10:46:58 +0200

    fix ui styles
    code colors
    
    mobile version UI

--- docs/docs/.vuepress/theme/components/Navbar.vue
+++ docs/docs/.vuepress/theme/components/Navbar.vue
@@ -17,7 +17,7 @@
         ref="siteName"
         class="site-name"
         :class="{ 'can-hide': $site.themeConfig.logo }"
-      >{{ $siteTitle }}</span>
+      >biome<span>.text</span></span>
     </RouterLink>
 
     <div
