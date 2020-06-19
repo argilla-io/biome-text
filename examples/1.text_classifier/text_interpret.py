6e1f6f56900e0fb4963bfbda7065d19cf0ea7df0
Francisco Aranda <francisco@recogn.ai>
2020-06-19 10:39:02 +0200

    remove examples

--- examples/1.text_classifier/text_interpret.py
+++
@@ -1,6 +0,0 @@
-from biome.text import Pipeline
-
-if __name__ == "__main__":
-    trained_pl = Pipeline.from_pretrained("experiment/model.tar.gz")
-    print(trained_pl.explain(text="The simple file"))
-    trained_pl.serve()
