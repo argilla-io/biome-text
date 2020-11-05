22541a52d6ad3ead7138bb52d422ca3bed49a7b6
ignacioct <ignaciotalaveracepeda@gmail.com>
2020-11-05 16:45:28 +0100

    second test deleted

--- tests/text/test_pipeline_explore.py
+++ tests/text/test_pipeline_explore.py
@@ -18,28 +18,8 @@ def test_explore_creation(dataset):
     pl = Pipeline.from_config(
         {"name": "test", "head": {"type": "TextClassification", "labels": ["a"]}}
     )
-    explore_id = explore.create(
-        pl, dataset, explore_id="mock", show_explore=False
-    )
+    explore_id = explore.create(pl, dataset, explore_id="mock", show_explore=False)
     elastic = Elasticsearch()
     explore_meta = elastic.get(index=".biome", id=explore_id)["_source"]
     assert pl.config.as_dict() == explore_meta["pipeline_config"]
-
-
-def test_explore_with_no_mapping_in_ds(monkeypatch, dataset_without_mapping):
-    """Test the creation of the ElasticSearch instance without mapping"""
-
-    def _explore(explore_id, pipeline, dataset, options, es_dao):
-        return dataset.to_mapped_dataframe().compute()
-
-    def _show(explore_id, es_host):
-        return None
-
-    monkeypatch.setattr(explore, "_explore", _explore)
-    monkeypatch.setattr(explore, "show", _show)
-
-    pl = Pipeline.from_config(
-        {"name": "test", "head": {"type": "TextClassification", "labels": ["a"]}}
-    )
-    explore_id = explore.create(pl, dataset_without_mapping)
     assert len(explore_id) > 0
