4791385cdb96d25178e5b31d4c69260f778718e2
ignacioct <ignaciotalaveracepeda@gmail.com>
2020-11-06 17:28:37 +0100

    test csv source

--- tests/text/data/test_csv_source.py
+++ tests/text/data/test_csv_source.py
@@ -1,17 +1,26 @@
-from biome.text.data import DataSource
 import os
 
-from tests import DaskSupportTest, RESOURCES_PATH
+import pytest
+from biome.text import Dataset
+
+from tests import RESOURCES_PATH
 
 FILES_PATH = os.path.join(RESOURCES_PATH, "data")
 
 
-class CsvDatasourceTest(DaskSupportTest):
-    def test_reader_csv_with_leading_and_trailing_spaces_in_examples(self):
-        ds = DataSource(
-            format="csv",
-            source=os.path.join(FILES_PATH, "trailing_coma_in_headers.csv"),
-            sep=";",
-        )
-        df = ds.to_dataframe().compute()
-        self.assertIn("name", df.columns)
+@pytest.fixture
+def dataset() -> Dataset:
+    """Creation of dataset"""
+
+    file_path = os.path.join(FILES_PATH, "trailing_coma_in_headers.csv")
+
+    # TODO: erase download mode once the release which includes not caching the datasets is out
+    dataset = Dataset.from_csv(paths=file_path, delimiter=";")
+
+    return dataset
+
+
+def test_reader_csv_with_leading_and_trailing_spaces_in_examples(dataset):
+    """Asserting that the csv with ; delimiter is correctly converted into dataset"""
+
+    assert " name" in dataset.column_names
