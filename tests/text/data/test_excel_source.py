fa69fed79fe26a43364f69ed5f8eaf6691fc540c
ignacioct <ignaciotalaveracepeda@gmail.com>
2020-11-06 17:13:02 +0100

    test excel source

--- tests/text/data/test_excel_source.py
+++ tests/text/data/test_excel_source.py
@@ -3,6 +3,8 @@ import os
 import pandas as pd
 import pytest
 from biome.text import Dataset
+from datasets.features import Features
+from datasets.features import Value
 
 from tests import RESOURCES_PATH
 
@@ -10,7 +12,18 @@ FILES_PATH = os.path.join(RESOURCES_PATH, "data")
 
 
 @pytest.fixture
-def dataset() -> Dataset:
+def features_dict() -> dict:
+    """Creating the features dictionary"""
+    str_value = Value("string")
+    int_value = Value("int64")
+    features = Features(
+        Notification=int_value, Type=str_value, Plant=int_value, Serial=str_value
+    )
+    return features
+
+
+@pytest.fixture
+def dataset(features_dict) -> Dataset:
     """
     Creation of dataset
     Excel into dataset via pandas
@@ -19,9 +32,7 @@ def dataset() -> Dataset:
     file_path = os.path.join(FILES_PATH, "test.xlsx")
     df = pd.read_excel(file_path)
 
-    # Dropping problematic columns that has NaN values
-    df = df[["Notification", "Notification type"]]
-    return Dataset.from_pandas(df)
+    return Dataset.from_pandas(df, features=features_dict)
 
 
 def test_read_excel(dataset):
