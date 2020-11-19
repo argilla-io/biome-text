e1054cbc5a3e0dd755ba668ef9869cdd799768f3
dcfidalgo <david@recogn.ai>
2020-11-19 13:45:54 +0100

    remove data folder

--- src/biome/text/data/__init__.py
+++
@@ -1,8 +0,0 @@
-from typing import Union
-
-from allennlp.data import AllennlpDataset, AllennlpLazyDataset
-
-from .datasource import DataSource
-
-
-InstancesDataset = Union[AllennlpDataset, AllennlpLazyDataset]
