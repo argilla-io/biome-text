007efb5a5b4e00df8f2a9dea97bb7cd86486e2c2
dcfidalgo <david@recogn.ai>
2020-11-27 12:01:04 +0100

    improve cli help and make cli commands uniform

--- src/biome/text/cli/serve.py
+++ src/biome/text/cli/serve.py
@@ -4,18 +4,28 @@ from click import Path
 from biome.text import Pipeline
 
 
-@click.command("serve", help="Serves pipeline as rest api service")
+@click.command()
 @click.argument("pipeline_path", type=Path(exists=True))
-@click.option("-p", "--port", "port", type=int, default=8888, show_default=True)
 @click.option(
-    "--predictions-dir",
-    "predictions_dir",
-    type=str,
-    default=None,
+    "--port",
+    "-p",
+    type=int,
+    default=8888,
     show_default=True,
+    help="Port on which to serve the REST API.",
+)
+@click.option(
+    "--predictions_dir",
+    "-pd",
+    type=click.Path(),
+    default=None,
     help="Path to log raw predictions from the service.",
 )
 def serve(pipeline_path: str, port: int, predictions_dir: str) -> None:
+    """Serves the pipeline predictions as a REST API
+
+    PIPELINE_PATH is the path to a pretrained pipeline (model.tar.gz file).
+    """
     pipeline = Pipeline.from_pretrained(pipeline_path)
 
     if predictions_dir:
