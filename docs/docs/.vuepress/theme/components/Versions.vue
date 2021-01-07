e4532f4e361504ca450c2191619853d33df7c638
leiyre <leire@recogn.ai>
2021-01-07 14:39:08 +0100

    style versioning component (#489)
    * style versioning component
    
    * format stylus
    
    * fix trailing whitespaces
    
    * fix security alert from github
    
    * move axios dependency from devDependencies to dependencies
    
    Co-authored-by: dcfidalgo <david@recogn.ai>

--- docs/docs/.vuepress/theme/components/Versions.vue
+++ docs/docs/.vuepress/theme/components/Versions.vue
@@ -1,11 +1,13 @@
 <template>
-  <span class="nav-item" v-if="options && options.length > 0">
-    Version:
-    <select v-model="selected" @change="onChange">
-      <option v-for="option in options" :value="option">
-        {{ option }}
-      </option>
-    </select>
+  <span class="nav-versions" v-if="options && options.length > 0">
+    <div class="nav-versions__select" v-model="selected" @click="showOptions = true"><strong>{{selected}}</strong></div>
+    <div class="nav-versions__options__container" v-if="showOptions">
+      <ul class="nav-versions__options">
+        <li class="nav-versions__option" @click="onChange(option)" v-for="option in options" :value="option">
+          <a :class="option === selected ? 'active' : ''" href="#">{{ option }}</a>
+        </li>
+      </ul>
+    </div>
   </span>
 </template>
 
@@ -17,7 +19,8 @@ export default {
   data() {
     return {
       selected: undefined,
-      options: []
+      options: [],
+      showOptions: false,
     };
   },
   created: async function() {
@@ -43,7 +46,9 @@ export default {
     } catch (ex) {}
   },
   methods: {
-    onChange(event) {
+    onChange(option) {
+      this.showOptions = false;
+      this.selected = option;
       const targetVersionsPath =
         this.selected === 'master' ? '/' : `/versions/${this.selected}/`;
       const paths = window.location.pathname.split('/')
@@ -63,3 +68,48 @@ export default {
   }
 };
 </script>
+<style lang="stylus">
+
+.nav-versions
+  display: block
+  margin: auto
+  text-align: center
+  position: relative
+  &__select
+    background: transparent
+    min-height: 30px
+    padding: 0.5em
+    color: $textColor
+    font-size: 15px
+    &::after
+      content: ''
+      border-left: 4px solid transparent
+      border-right: 4px solid transparent
+      border-top: 6px solid $arrowBgColor
+      border-bottom: 0
+      display: inline-block
+      margin-left: 0.5em
+  &__options
+    background: white
+    border: 1px solid $borderColor
+    display: inline-block
+    width: auto
+    list-style: none
+    border-radius: 4px
+    &__container
+      position: absolute
+      top: 2em
+      left: 0
+      right: 0
+      margin: auto !important
+  &__option
+    padding: 0em 0.5em
+    font-size: 14px
+    font-size: 15px
+    text-align: left
+    a
+      color: $textColor
+      &:hover, &:focus, &.active
+        color: $accentColor
+
+</style>
