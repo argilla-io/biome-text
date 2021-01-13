a56509dccf92bc4483a79a4b37589d3dfeb158c3
leiyre <leire@recogn.ai>
2021-01-13 15:11:32 +0100

    resize to responsive mode on Mobile  Firefox (#495)

--- docs/docs/.vuepress/config.js
+++ docs/docs/.vuepress/config.js
@@ -20,6 +20,7 @@ module.exports = {
   title: 'biome.text',
   description: 'biome.text practical NLP open source library.',
   head: [
+    ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1.0' }],
     ['link', { rel: "shortcut icon", href: "/favicon.ico"}],
     ['meta', { property: 'og:image', content: 'https://www.recogn.ai/images/biome_og.png' }],
   ],
