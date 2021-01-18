c12a8129eeba6d944643bf9d8fb497ac6c08eeaa
David Fidalgo <david@recogn.ai>
2021-01-18 10:46:37 +0100

    Log wandb data to `.wandb` (#499)
    * log to .wandb instead of wandb to avoid importing the folder instead of the module
    
    * order dependencies alphabetically, use packaging.version, include wandb as test dependency
    
    * remove unnecessary import
    
    * add wandblogger test
    
    * remove packaging dependency

--- setup.py
+++ setup.py
@@ -1,34 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import glob
-import os
+
+from pip import __version__ as pip_version
+from pkg_resources import parse_version
+from setuptools import setup
 
 try:
     from setuptools import find_namespace_packages
-    from setuptools import setup
 except ImportError as error:
     raise ImportError("Make sure you have setuptools >= 40.1.0 installed!") from error
-
-from pip import __version__ as pip_version
 
 # We require a fairly new version to make use of the new dependency resolver!
 REQUIRED_PIP_VERSION = "20.3.0"
 
 
-def check_pip_version():
-    def version2int(version: str) -> int:
-        version_fractions = [f"{int(n):02d}" for n in version.split(".")]
-        return int("".join(version_fractions))
-
-    if version2int(pip_version) < version2int(REQUIRED_PIP_VERSION):
+if __name__ == "__main__":
+    if parse_version(pip_version) <= parse_version(REQUIRED_PIP_VERSION):
         raise OSError(
             f"Minimal required pip version is {REQUIRED_PIP_VERSION}, found: {pip_version}\n"
             "Please upgrade pip: pip install --upgrade pip"
         )
-
-
-if __name__ == "__main__":
-    check_pip_version()
 
     setup(
         name="biome-text",
@@ -45,28 +37,28 @@ if __name__ == "__main__":
         package_dir={"": "src"},
         install_requires=[
             "allennlp~=1.3.0",
-            "spacy~=2.3.0",
-            "gevent~=20.9.0",
+            "beautifulsoup4~=4.9.0",
+            "cachey~=0.2.0",
+            "captum~=0.2.0",
+            "click~=7.1.0",
+            "datasets~=1.1.2",
+            "distributed~=2.17.0",
+            "elasticsearch>=6.8.0,<7.5.0",
+            "fastapi~=0.55.0",
             "flask~=1.1.2",
             "flask-cors~=3.0.8",
-            "click~=7.1.0",
-            "beautifulsoup4~=4.9.0",
-            "lxml~=4.6.2",
-            "fastapi~=0.55.0",
-            "uvicorn~=0.11.0",
-            "distributed~=2.17.0",
-            "cachey~=0.2.0",
-            "pandas~=1.1.0",
-            "xlrd~=1.2.0",
             "flatdict~=4.0.0",
-            "s3fs~=0.4.0",
-            "captum~=0.2.0",
+            "gevent~=20.9.0",
             "ipywidgets~=7.5.1",
+            "lxml~=4.6.2",
             "mlflow~=1.9.0",
-            "elasticsearch>=6.8.0,<7.5.0",
+            "pandas~=1.1.0",
             "ray[tune]~=1.0.0",
-            "datasets~=1.1.2",
+            "s3fs~=0.4.0",
+            "spacy~=2.3.0",
             "tqdm>=4.49.0",
+            "uvicorn~=0.11.0",
+            "xlrd~=1.2.0",
         ],
         extras_require={
             "dev": [
@@ -75,6 +67,7 @@ if __name__ == "__main__":
                 "pytest-cov>=2.10.0",
                 "pytest-pylint>=0.14.0",
                 "pytest-notebook~=0.6.0",
+                "wandb>=0.10.12",
                 # documentation
                 "pdoc3~=0.8.1",
                 # development
