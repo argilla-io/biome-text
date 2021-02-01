745cff3c3e88d1244721ff992ef37307c0fbfd6d
dcfidalgo <david@recogn.ai>
2021-02-01 15:49:57 +0100

    add module docstring

--- src/biome/text/mlflow_model.py
+++ src/biome/text/mlflow_model.py
@@ -1,3 +1,8 @@
+"""Docstring for the mlflow_model.py module
+
+This module is only used by MLFlow internally for loading a Pipeline as MLFlow model from an MLFlow Tracking server.
+"""
+
 import pandas as pd
 
 from biome.text import Pipeline
