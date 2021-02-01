f463d10e79c9926a2e271b4335a5b9b3abd9abd9
dcfidalgo <david@recogn.ai>
2021-02-01 15:13:04 +0100

    batch predictions now always return lists

--- src/biome/text/mlflow_model.py
+++ src/biome/text/mlflow_model.py
@@ -10,9 +10,6 @@ class BiomeTextModel:
     def predict(self, dataframe: pd.DataFrame):
         batch = dataframe.to_dict(orient="records")
         predictions = self.pipeline.predict(batch=batch)
-        # covering the case when batch size is 1
-        if not isinstance(predictions, list):
-            predictions = [predictions]
 
         return pd.DataFrame(predictions)
 
