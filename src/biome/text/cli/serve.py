cf3239bed6f5ce3bb7f27f126caa64e8fc3ad04f
dcfidalgo <david@recogn.ai>
2021-02-03 13:28:27 +0100

    add BaseModels for input validation + improved error messages

--- src/biome/text/cli/serve.py
+++ src/biome/text/cli/serve.py
@@ -1,14 +1,23 @@
+import inspect
 from typing import Any
+from typing import Callable
 from typing import Dict
+from typing import List
+from typing import Optional
+from typing import Tuple
 
 import click
 import uvicorn
 from allennlp.common.util import sanitize
 from click import Path
 from fastapi import FastAPI
+from fastapi import HTTPException
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import PlainTextResponse
+from pydantic import create_model
+from starlette.exceptions import HTTPException as StarletteHTTPException
 
 from biome.text import Pipeline
-from biome.text.errors import http_error_handling
 
 
 @click.command()
@@ -44,14 +53,69 @@ def serve(pipeline_path: str, port: int, predictions_dir: str) -> None:
 
 def _serve(pipeline: Pipeline, port: int):
     """Serves an pipeline as rest api"""
+    predict_parameters = inspect.signature(pipeline.predict).parameters
+    model_parameters = {
+        name: (
+            par.annotation,
+            None,  # We need a default value to allow for batch predictions!
+        )
+        for name, par in predict_parameters.items()
+        if par.default == inspect.Parameter.empty
+    }
+    optional_parameters = {
+        name: (par.annotation, par.default)
+        for name, par in predict_parameters.items()
+        # The batch parameter needs an extra logic to allow for a proper BaseModel for it
+        if par.default != inspect.Parameter.empty and name != "batch"
+    }
+
+    class Config:
+        extra = "forbid"
+
+    ModelInput = create_model("ModelInput", **model_parameters, __config__=Config)
+    PredictInput = create_model(
+        "PredictInput",
+        **model_parameters,
+        batch=(List[ModelInput], None),
+        **optional_parameters,
+        __config__=Config,
+    )
+
+    class http_error_handling:
+        """Error handling for http error transcription"""
+
+        def __enter__(self):
+            pass
+
+        def __exit__(self, exc_type, exc_val, exc_tb):
+            if isinstance(exc_val, Exception):
+                # Common http error handling
+                raise HTTPException(status_code=500, detail=str(exc_val))
 
     def make_app() -> FastAPI:
         app = FastAPI()
 
+        error_msg = f". Check the docs at '0.0.0.0:{port}/docs' for an example of a valid request body."
+
+        @app.exception_handler(RequestValidationError)
+        async def validation_exception_handler(request, exc):
+            return PlainTextResponse(str(exc) + error_msg, status_code=400)
+
+        @app.exception_handler(StarletteHTTPException)
+        async def http_exception_handler(request, exc):
+            if exc.status_code == 400:
+                return PlainTextResponse(
+                    str(exc.detail) + error_msg, status_code=exc.status_code
+                )
+            else:
+                return PlainTextResponse(str(exc.detail), status_code=exc.status_code)
+
         @app.post("/predict")
-        async def predict(inputs: Dict[str, Any]):
+        async def predict(predict_input: PredictInput):
             with http_error_handling():
-                return sanitize(pipeline.predict(**inputs))
+                return sanitize(
+                    pipeline.predict(**predict_input.dict(skip_defaults=True))
+                )
 
         @app.get("/_config")
         async def config():
