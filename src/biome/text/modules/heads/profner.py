7b8c150e3e6df2d839efe91b05e7324ac25671d4
dcfidalgo <david@recogn.ai>
2021-02-09 22:11:35 +0100

    First working version with tests

--- src/biome/text/modules/heads/profner.py
+++ src/biome/text/modules/heads/profner.py
@@ -1,13 +1,39 @@
+import dataclasses
+from typing import Dict
 from typing import List
 from typing import Optional
+from typing import Tuple
+from typing import Union
 
+import numpy
+import numpy as np
+import torch
 from allennlp.data import Instance
+from allennlp.data import Token
+from allennlp.data.fields import ArrayField
+from allennlp.data.fields import LabelField
+from allennlp.data.fields import SequenceLabelField
+from allennlp.data.fields import TextField
+from allennlp.modules import ConditionalRandomField
+from allennlp.modules import TimeDistributed
+from allennlp.modules.conditional_random_field import allowed_transitions
+from allennlp.nn.util import get_text_field_mask
+from allennlp.training.metrics import CategoricalAccuracy
+from allennlp.training.metrics import FBetaMeasure
+from allennlp.training.metrics import SpanBasedF1Measure
+from torch import BoolTensor
+from torch import Tensor
 from transformers import AutoTokenizer
+from transformers import PreTrainedTokenizer
+from transformers import PreTrainedTokenizerFast
 
 from biome.text.backbone import ModelBackbone
+from biome.text.featurizer import FeaturizeError
 from biome.text.modules.configuration import ComponentConfiguration
-from biome.text.modules.configuration import FeedForwardConfiguration
-from biome.text.modules.heads import TaskHead
+from biome.text.modules.configuration import Seq2VecEncoderConfiguration
+from biome.text.modules.heads.task_head import TaskHead
+from biome.text.modules.heads.task_prediction import SENTINEL
+from biome.text.modules.heads.task_prediction import TaskPrediction
 
 
 class ProfNer(TaskHead):
@@ -15,28 +41,71 @@ class ProfNer(TaskHead):
         self,
         backbone: ModelBackbone,
         classification_labels: List[str],
+        classification_pooler: Seq2VecEncoderConfiguration,
         ner_tags: List[str],
-        transformers_model: Optional[str] = None,
-        label_encoding: Optional[str] = "BIOUL",
-        top_k: int = 1,
-        dropout: Optional[float] = 0.0,
-        feedforward: Optional[FeedForwardConfiguration] = None,
+        ner_tags_encoding: str,
+        transformers_model: str,
     ) -> None:
         super().__init__(backbone)
 
-        self._transformers_model = transformers_model
+        if ner_tags_encoding not in ["BIOUL", "BIO"]:
+            raise ValueError(
+                f"NER tags encoding '{ner_tags_encoding}' not supported. Allowed values are ['BIOUL', 'BIO']"
+            )
 
-    def featurize(self, text: List[str], labels: str, tags: List[str]) -> Instance:
+        self._transformer_tokenizer: Union[
+            PreTrainedTokenizer, PreTrainedTokenizerFast
+        ] = AutoTokenizer.from_pretrained(transformers_model)
+
+        self.backbone.vocab.add_tokens_to_namespace(ner_tags, "ner_tags")
+        self.backbone.vocab.add_tokens_to_namespace(
+            classification_labels, "classification_labels"
+        )
+
+        self._classification_pooler = classification_pooler.input_dim(
+            self.backbone.encoder.get_output_dim()
+        ).compile()
+        self._classification_layer = torch.nn.Linear(
+            self._classification_pooler.get_output_dim(), len(classification_labels)
+        )
+
+        self._classification_loss = torch.nn.CrossEntropyLoss()
+
+        self._tag_layer = TimeDistributed(
+            torch.nn.Linear(self.backbone.encoder.get_output_dim(), len(ner_tags))
+        )
+
+        constraints = allowed_transitions(
+            ner_tags_encoding,
+            self.backbone.vocab.get_index_to_token_vocabulary("ner_tags"),
+        )
+        self._crf = ConditionalRandomField(len(ner_tags), constraints)
+
+        self.metrics = {
+            "classification_accuracy": CategoricalAccuracy(),
+            "classification_micro": FBetaMeasure(average="micro"),
+            "classification_macro": FBetaMeasure(average="macro"),
+            "ner_f1": SpanBasedF1Measure(
+                self.backbone.vocab, "ner_tags", label_encoding=ner_tags_encoding
+            ),
+        }
+
+    def featurize(
+        self,
+        tokens: List[str],
+        labels: Optional[str] = None,
+        tags: Optional[List[str]] = None,
+    ) -> Instance:
         """
 
         Parameters
         ----------
-        text
-            pretokenized input
+        tokens
+            spacy tokenized input
         labels
-            label
+            The classification label
         tags
-            BIO tags
+            The NER tags
 
         Returns
         -------
@@ -46,11 +115,214 @@ class ProfNer(TaskHead):
         ------
         FeaturizeError
         """
-        if self._transformers_model:
-            tokenizer = AutoTokenizer.from_pretrained(self._transformers_model)
-            input_ids = tokenizer(
-                text, is_split_into_words=True, return_offsets_mapping=True
+        if not isinstance(tokens, list):
+            raise FeaturizeError("Input argument 'tokens' has to be a list of strings")
+        if (labels is not None and tags is None) or (
+            labels is None and tags is not None
+        ):
+            raise FeaturizeError("You are missing either labels or tags!")
+
+        input_ids = self._transformer_tokenizer(
+            tokens,
+            is_split_into_words=True,
+            return_offsets_mapping=True,
+            return_special_tokens_mask=True,
+        )
+        transformer_tokens_str = self._transformer_tokenizer.convert_ids_to_tokens(
+            input_ids["input_ids"]
+        )
+        # We only want to tag the first word piece of the word tokens
+        word_piece_mask = np.array(input_ids["offset_mapping"], dtype=int)[:, 0] == 0
+        ner_tokens_mask = word_piece_mask & ~np.array(
+            input_ids["special_tokens_mask"], dtype=bool
+        )
+
+        instance = self.backbone.featurizer(
+            transformer_tokens_str, to_field="subtokens", aggregate=True, tokenize=False
+        )
+
+        array_field = ArrayField(ner_tokens_mask, padding_value=0, dtype=bool)
+        instance.add_field("ner_subtokens_mask", array_field)
+
+        if labels is not None and tags is not None:
+            label_field = LabelField(labels, label_namespace="classification_labels")
+            instance.add_field("labels", label_field)
+
+            text_field = TextField(
+                [Token(token) for token in tokens],
+                token_indexers={},
             )
+            try:
+                sequence_label_field = SequenceLabelField(
+                    tags,
+                    sequence_field=text_field,
+                    label_namespace="ner_tags",
+                )
+            except Exception as error:
+                raise FeaturizeError(
+                    f"Could not create SequenceLabelField for: {tokens, tags}"
+                ) from error
+            else:
+                instance.add_field("tags", sequence_label_field)
+
+        return instance
+
+    def forward(
+        self,
+        subtokens: Dict[str, Union[Tensor, Dict[str, Tensor]]],
+        ner_subtokens_mask: BoolTensor,
+        tokens: Dict[str, Union[Tensor, Dict[str, Tensor]]] = None,
+        labels: Tensor = None,
+        tags: Tensor = None,
+    ):
+        """
+
+        Parameters
+        ----------
+        subtokens
+            Subword tokens produced by the transformers tokenizer
+        ner_subtokens_mask
+            Masks the first subtoken belonging to a word token
+        tokens
+            Word tokens produced by the spacy tokenizer
+        labels
+            Classification label
+        tags
+            NER tags for the word tokens
+
+        Returns
+        -------
+
+        """
+        # This is the mask for padded word pieces
+        mask = get_text_field_mask(subtokens)
+        embedded_tokens = self.backbone(subtokens, mask)
+
+        ner_logits = self._tag_layer(embedded_tokens)
+        classification_logits = self._classification_layer(
+            self._classification_pooler(embedded_tokens, mask)
+        )
+
+        viterbi_paths: List[Tuple[List[int], float]] = self._crf.viterbi_tags(
+            ner_logits, ner_subtokens_mask
+        )
+
+        output = dict(
+            classification_logits=classification_logits, viterbi_paths=viterbi_paths
+        )
+
+        if labels is not None and tags is not None:
+            # Classification loss
+            output["loss"] = self._classification_loss(classification_logits, labels)
+
+            # NER loss
+
+            # sort the ner_tokens_mask with all Trues first
+            # pytorch has no stable sort, so we need a little trick
+            trick = (
+                torch.arange(
+                    0,
+                    1,
+                    1 / ner_subtokens_mask.numel(),
+                    device=ner_subtokens_mask.device,
+                )  # in this way we avoid equal elements when sorting => no stable sort necessary
+                .flip(-1)  # we want to sort with descending=True in the end
+                .view(ner_subtokens_mask.size())
+            )
+            tags_mask, mask_indices = (ner_subtokens_mask + trick).sort(descending=True)
+            # retype back to bool and truncate to the size of the tags
+            tags_mask = tags_mask.int().bool()[:, : tags.size(-1)]
+
+            # sort the ner_logits according to the mask indices
+            first_index = torch.arange(
+                ner_logits.size(0), device=ner_logits.device
+            ).unsqueeze(1)
+            ner_logits_sorted = ner_logits[first_index, mask_indices, :]
+            # truncate to the size of the tags
+            ner_logits_sorted = ner_logits_sorted[:, : tags.size(-1)]
+
+            output["loss"] += -1 * self._crf(ner_logits_sorted, tags, tags_mask)
+
+            # metrics
+            self.metrics["classification_accuracy"](classification_logits, labels)
+            self.metrics["classification_micro"](classification_logits, labels)
+            self.metrics["classification_macro"](classification_logits, labels)
+            self.metrics["ner_f1"](ner_logits_sorted, tags, tags_mask)
+
+        return output
+
+    def _make_task_prediction(
+        self,
+        single_forward_output: Dict[str, numpy.ndarray],
+        instance: Instance,
+    ) -> TaskPrediction:
+
+        # softmax is not implemented in numpy ...
+        classification_logits = torch.from_numpy(
+            single_forward_output["classification_logits"]
+        )
+        classification_probabilities = (
+            torch.nn.functional.softmax(classification_logits, dim=0)
+            .sort(descending=True)[0]
+            .tolist()
+        )
+        classification_labels = [
+            self.backbone.vocab.get_token_from_index(int(ind), "classification_labels")
+            for ind in classification_logits.argsort(descending=True)
+        ]
+        ner_tags = [
+            self.backbone.vocab.get_token_from_index(ind, "ner_tags")
+            for ind in single_forward_output["viterbi_paths"][0]
+        ]
+
+        return ProfNerPrediction(
+            classification_labels=classification_labels,
+            classification_probabilities=classification_probabilities,
+            ner_tags=ner_tags,
+        )
+
+        pass
+
+    def get_metrics(self, reset: bool = False) -> Dict[str, float]:
+        metrics = {
+            "classification/accuracy": self.metrics[
+                "classification_accuracy"
+            ].get_metric(reset)
+        }
+        for key, value in (
+            self.metrics["classification_micro"].get_metric(reset).items()
+        ):
+            metrics.update({f"classification/micro_{key}": value})
+        for key, value in (
+            self.metrics["classification_macro"].get_metric(reset).items()
+        ):
+            metrics.update({f"classification/macro_{key}": value})
+        for key, value in self.metrics["ner_f1"].get_metric(reset).items():
+            metrics.update({f"ner/{key}": value})
+
+        return metrics
+
+
+@dataclasses.dataclass
+class ProfNerPrediction(TaskPrediction):
+    """Output dataclass for the `ProfNer` head
+
+    Parameters
+    ----------
+    classification_labels
+        Ordered list of predictions, from the label with the highest to the label with the lowest probability.
+    classification_probabilities
+        Ordered list of probabilities, from highest to lowest probability.
+    ner_tags
+        NER tags for the input
+    tokens
+        Tokens of the tokenized input
+    """
+
+    classification_labels: List[str]
+    classification_probabilities: List[float]
+    ner_tags: List[str]
+    tokens: Optional[List[Token]] = SENTINEL
 
 
 class ProfNerConfiguration(ComponentConfiguration[ProfNer]):
---
+++ tests/text/modules/heads/test_profner.py
@@ -0,0 +1,76 @@
+import itertools
+
+import pytest
+from allennlp.data import Batch
+from allennlp.data import Instance
+from allennlp.data import Vocabulary
+from allennlp.data.fields import TextField
+
+from biome.text import Dataset
+from biome.text import Pipeline
+
+
+@pytest.fixture
+def dataset() -> Dataset:
+    data = {
+        "tokens": [
+            ["Test", "this"],
+            ["superlongword", "for", "the", "transformer", "tokenizer"],
+        ],
+        "tags": [["O", "O"], ["B-TEST", "O", "B-TEST", "I-TEST", "I-TEST"]],
+        "labels": ["0", "1"],
+    }
+
+    return Dataset.from_dict(data)
+
+
+@pytest.fixture
+def pipeline_dict(dataset) -> dict:
+    model_name = "sshleifer/tiny-distilbert-base-cased"
+    model_name = "prajjwal1/bert-tiny"
+
+    return {
+        "name": "test_prof_ner",
+        "features": {
+            "transformers": {
+                "model_name": model_name,
+                "trainable": True,
+            }
+        },
+        "head": {
+            "type": "ProfNer",
+            "classification_labels": dataset.unique("labels"),
+            "classification_pooler": {
+                "type": "bert_pooler",
+                "pretrained_model": model_name,
+                "requires_grad": True,
+                "dropout": 0.1,
+            },
+            "ner_tags": list(set(itertools.chain.from_iterable(dataset["tags"]))),
+            "ner_tags_encoding": "BIO",
+            "transformers_model": model_name,
+        },
+    }
+
+
+def test_featurize(pipeline_dict: dict, dataset: Dataset, tmp_path):
+    pipeline = Pipeline.from_config(pipeline_dict)
+    pipeline.train(output=str(tmp_path / "test_output"), training=dataset)
+    predictions = pipeline.predict(
+        batch=[
+            {"tokens": ["Test", "this"]},
+            {"tokens": ["superlongword", "for", "the", "transformer", "tokenizer"]},
+        ],
+        add_tokens=True,
+    )
+    print(predictions)
+
+
+def test_null():
+    tf = TextField("", {})
+    instance = Instance({"tf": tf})
+    vocab = Vocabulary.from_instances([instance])
+    batch = Batch([instance])
+    batch.index_instances(vocab)
+
+    print(batch.as_tensor_dict())
