b5b64e9ee15ed5e4f7d10a7a430eb1bd68a3bbd3
dcfidalgo <david@recogn.ai>
2021-04-29 15:15:01 +0200

    bump up ray tune version

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ if __name__ == "__main__":
             "numpy",
             "pandas",
             "pytorch-lightning==1.3.0rc1",
-            "ray[tune]~=1.2.0",
+            "ray[tune]~=1.3.0",
             "spacy~=2.3.0",
             "torch",  # the version is defined by allennlp
             "transformers",  # the version is defined by allennlp
