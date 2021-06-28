a384d5a6e45ed3b79e639f627c7b07a34f7c9cac
dcfidalgo <david@recogn.ai>
2021-06-28 17:49:11 +0200

    adapt tests

--- tests/text/test_pipeline_predict.py
+++ tests/text/test_pipeline_predict.py
@@ -15,9 +15,11 @@ def pipeline() -> Pipeline:
 
 
 def test_return_empty_prediction_for_failed_prediction(pipeline):
-    fallback_pred = {"labels": [], "probabilities": []}
-    assert pipeline.predict("") == fallback_pred
-    assert pipeline.predict(batch=[{"text": ""}, {"text": ""}]) == [fallback_pred] * 2
+    empty_prediction = {"labels": [], "probabilities": []}
+    assert pipeline.predict("") == empty_prediction
+    assert (
+        pipeline.predict(batch=[{"text": ""}, {"text": ""}]) == [empty_prediction] * 2
+    )
 
 
 def test_batch_parameter_gets_ignored(pipeline):
@@ -55,7 +57,7 @@ def test_return_single_or_list(pipeline, monkeypatch):
         return [
             TextClassificationPrediction(labels=["a"], probabilities=[1])
             if i % 2 == 0
-            else None
+            else pipeline.head.empty_prediction
             for i, _ in enumerate(batch)
         ]
 
@@ -71,4 +73,7 @@ def test_return_single_or_list(pipeline, monkeypatch):
         batch=[{"text": "test"}, {"text": "no instance for this input"}]
     )
     assert isinstance(batch_prediction, list) and len(batch_prediction) == 2
-    assert isinstance(batch_prediction[0], dict) and batch_prediction[1] is None
+    assert (
+        isinstance(batch_prediction[0], dict)
+        and batch_prediction[1] == pipeline.head.empty_prediction.as_dict()
+    )
