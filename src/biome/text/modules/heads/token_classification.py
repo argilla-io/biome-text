06f0346603dc26b7985674258d02b1786e488560
dcfidalgo <david@recogn.ai>
2021-06-28 17:59:25 +0200

    remove superfluous import

--- src/biome/text/modules/heads/token_classification.py
+++ src/biome/text/modules/heads/token_classification.py
@@ -35,7 +35,6 @@ from biome.text.modules.configuration import FeedForwardConfiguration
 from biome.text.modules.heads.task_head import TaskHead
 from biome.text.modules.heads.task_head import TaskName
 from biome.text.modules.heads.task_prediction import Entity
-from biome.text.modules.heads.task_prediction import TaskPrediction
 from biome.text.modules.heads.task_prediction import TokenClassificationPrediction
 
 
